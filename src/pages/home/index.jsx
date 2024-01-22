8c4cac0793ed4bfb7e526d0ae0fa48c8adf00ab9
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-01-22 19:54:33 +0700

    fix: update hero section at home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Hero from "../../components/layouts/Hero";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowRotateLeft,
@@ -40,7 +39,15 @@ const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <Hero />
+      <div
+        className={`py-5 lg:py-40 bg-cover bg-center ${"bg-[url('/images/hero-bg.jpg')]"}`}
+      >
+        <div className="container px-8 py-24 flex flex-wrap">
+          <h1 className="w-full self-center font-bold text-white text-4xl md:text-7xl lg:w-1/2">
+            STYLIST PICKS BEAT THE HEAT
+          </h1>
+        </div>
+      </div>
       <MainContent
         title="Discover NEW Arrivals"
         tagline="Recently added shirts!"
